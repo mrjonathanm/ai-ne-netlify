12832ae50e217c09fb1cf1be94c977897bcf93a0
Jonathan Miles <mrjonathanmiles@gmail.com>
2025-06-17 08:40:16 +0100

    come on

---
+++ jm-uk/sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'jm-uk',
+
+  projectId: 'f8463tkj',
+  dataset: 'ai-ne',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
